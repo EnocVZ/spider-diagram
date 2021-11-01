3c09e521c316e05940151550b0479007e418dbfc
Enoc vazquez zaraoz <enoch.95@hotmail.com>
2021-11-01 03:36:06 -0600

    publicacion de diagrama

--- src/client/views/home.js
+++ src/client/views/home.js
@@ -4,39 +4,64 @@ import './stylr.css';
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { setting: "" };
-  }
-  componentDidMount() {
-    var cx = 300;
-    var cy = 250;
-    var r = 70;
-    var lados = 6;
-    this.setState({ setting2: this.points(cx, cy, r, lados, 0) });
-    this.setState({ setting3: this.points(cx, cy, r, lados, 10) });
-
-
-    this.calcPoints(
-      3, 400, 3
-    );
-  }
-  points(cx, cy, r, lados, suma) {
-    var a = 360 / lados;
-    // inicia la variable points
-    var points = (cx + r) + "," + cy + " ";
-    // calcula el resto de los puntos
-    for (var i = 1; i <= lados; i++) {
-      var aRad = (Math.PI / 180) * (a * i),
-        Xp = cx + r * Math.cos(aRad) + suma,
-        Yp = cy + r * Math.sin(aRad) - suma;
-
-      points += Xp + "," + Yp + " ";
-
-    }
-    // devuelve la variable points			
-    return points
   }
 
-  calcPoints = (circleRadius, size, vertices) => {
+  render() {
+    return <>
+      <RenderEl />
+    </>
+  }
+}
+
+class RenderEl extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
+
+
+  render() {
+    const data = [{ title: "Actividad fisica", cantidad: 0 },
+  { title: "Alcohol", cantidad: 3 },
+  { title: "Tabaco", cantidad: 3 },
+  { title: "IMC", cantidad: 1 },
+  { title: "Tensión Arterial", cantidad: 2 },
+  { title: "LDL", cantidad: 1 },
+  { title: "Glicemia", cantidad: 3 }];
+
+    return (
+      <React.Fragment>
+        <div className='App-wrapper'>
+          <Svg
+            width={250}
+            height={300}
+            circleRadius={200}
+            setting={{
+              vertice: data.length,
+              ringNumber: 3,
+              radius: 40, //si aumenta el numero de anillo disminuir el radio
+              ringColors: ["#ffb3b3", "#fbd8c2", "#b2e7cb",]
+            }}
+            list={[1, 2, 3]}
+            data={data}
+          />
+
+        </div>
+      </React.Fragment>
+
+    )
+  }
+}
+const Svg = ({
+  circleRadius,
+  width,
+  height,
+  setting,
+  list,
+  data
+}) => {
+
+  const calcPoints = (circleRadius, size, vertices) => {
     const RAD = Math.PI / 180;
     const TWO_PI = Math.PI * 2;
     const points = [];
@@ -44,132 +69,17 @@ export default class Home extends React.Component {
     const center = radius + circleRadius;
     const theta = TWO_PI / vertices;
     const angle = RAD * -90;
-    let pointsS = "";
-    let count = 1;
-    for (let i = 0; i < vertices; i++) {
+
+    for (var i = 0; i < vertices; i++) {
       let x = radius * Math.cos(theta * i + angle) + center;
       let y = radius * Math.sin(theta * i + angle) + center;
       points.push([x, y]);
-      count++;
     }
-    points.forEach((xy, i) => {
-      if (i == 1) {
-        pointsS = `M${xy[0]},${xy[1]}L,`;
-      } else if ((i + 1) == vertices) {
-        pointsS += `${xy[0]},${xy[1]}Z`;
-      } else {
-        pointsS += `${xy[0]},${xy[1]}L,`;
-      }
-    })
-    this.setState({
-      setting: pointsS
-    });
+
+    return points;
   };
-  render() {
 
-    return <>
-      <svg width="400" height="400">
-
-        <Poly2 sides={6} size={200} cx={100} cy={100} r={60} />
-        <Poly2 sides={6} size={200} cx={100} cy={100} r={80} />
-        <Poly2 sides={6} size={190} cx={100} cy={100} r={100} />
-        <Poly2 sides={6} size={190} cx={100} cy={100} r={120} />
-
-      </svg>
-      <RenderEl />
-    </>
-  }
-}
-
-const Poly2 = props => {
-  let points = [];
-  for (let i = 1; i < props.sides + 2; i++) {
-    points.push({
-      x:
-        props.cx +
-        Math.round(props.r * Math.sin((Math.PI / (props.sides / 2)) * i)),
-      y:
-        props.cy +
-        Math.round(props.r * Math.cos((Math.PI / (props.sides / 2)) * i))
-    });
-  }
-
-  let pointsStr = "";
-  points.forEach(val => {
-    pointsStr += `${val.x},${val.y} `;
-  });
-
-  return (
-
-    <>
-      <polyline points={pointsStr} fill="none" stroke="black" />
-    </>
-  );
-};
-
-
-const Poly = ({ pointsStr }) => {
-
-  return (
-    <>
-      <path stroke="#ccc" cx="300" cy="250" fill="none"
-        d={pointsStr}></path>
-    </>
-  );
-};
-
-
-
-class RenderEl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      vertices: 5,
-      circleRadius: 2,
-      size: 200,
-      showVertexDots: true,
-      showVertexLines: true,
-      points: [],
-    };
-  }
-
-
-
-
-  render() {
-    return (
-      <React.Fragment>
-        <div className='App-wrapper'>
-          <Svg
-            showVertexDots={true}
-            showVertexLines={true}
-            width={250}
-            height={200}
-            setting={{
-              vertice: 7,
-              ringNumber: 4,
-              radius: 40, //si aumenta el numero de anillo disminuir el radio
-              ringColors: ["#ffb3b3", "#fbd8c2", "#b2e7cb"]
-            }}
-          />
-
-        </div>
-      </React.Fragment>
-
-    )
-
-  }
-}
-const Svg = ({
-  circleRadius,
-  showVertexDots,
-  showVertexLines,
-  width,
-  height,
-  setting
-}) => {
-
-  const calcPoints = (circleRadius, size, vertices) => {
+  const calcTextPoints = (circleRadius, size, vertices) => {
     const RAD = Math.PI / 180;
     const TWO_PI = Math.PI * 2;
     const points = [];
@@ -191,23 +101,54 @@ const Svg = ({
   let polygonList;
   let radius = setting.radius;
   let pointText;
-  const data = [{ title: "Actividad fisica" },
-  { title: "Alcohol" },
-  { title: "Tabaco" },
-  { title: "IMC" },
-  { title: "Tensión Arterial" },
-  { title: "LDL" },
-  { title: "Glicemia" }];
   let segment = [];
-  return (
+  let pointListSegement = [];
 
+  const getData = () => {
+    return [...Array(setting.vertice)].map((e, vert) => {
+      let pointPos;
+      let radiusInternal = setting.radius;
+      let listData = [];
+      const ringList = [...Array(setting.ringNumber + 1)];
+      let reverseList = JSON.parse(JSON.stringify(list)).reverse();
+      let newListPoint = [];
+
+      ringList.forEach((e, i) => {
+        let point = calcPoints(radiusInternal, circleRadius, setting.vertice);
+        radiusInternal = (radiusInternal + 20);
+        listData.push(point[vert]);
+      });
+
+
+      for (let index = ringList.length - 1; index >= 0; index--) {
+        newListPoint.push(listData[index])
+      }
+
+      reverseList.push(0);
+
+      reverseList.forEach((el, number) => {
+        if (data[vert].cantidad > reverseList[number + 1] && data[vert].cantidad <= el) {
+          pointPos = listData[number];
+        }
+        if (data[vert].cantidad >= 0 && data[vert].cantidad < reverseList[reverseList.length - 2]) {
+          pointPos = listData[number];
+        }
+      });
+      pointListSegement.push(pointPos);
+      return <VertexDot circleRadius={1} key={vert} point={pointPos}
+        radius={width} />
+    });
+
+  }
+
+  return (
     <svg
       className='Svg'
       viewBox={`0 0 ${height} ${width}`}
-      xmlns='http://www.w3.org/2000/svg'
+      xmlns='http://www.w3.org/circleRadius0/svg'
     >
-      {[...Array(setting.ringNumber)].map((e, i) => {
-        let point = calcPoints(radius, 200, setting.vertice);
+      {[...Array(setting.ringNumber + 1)].map((e, i) => {
+        let point = calcPoints(radius, circleRadius, setting.vertice);
         radius = (radius + 20);
         if (i == 0) {
           polygonList = point;
@@ -216,54 +157,56 @@ const Svg = ({
         return <Polygon key={i} points={point.join(' ')}
           color={setting.ringColors[i] || "white"} />
       })}
-
-      {pointText = calcPoints(10, 200, 7)}
-      {showVertexLines ?
+      {pointText = calcTextPoints(20, circleRadius, setting.vertice)}
+      {
         polygonList.map((point, i) =>
-          <VertexLine key={i} point={point} radius={200} />
+          <VertexLine key={i} point={point} radius={circleRadius} />
         )
-        : null}
-
-      {showVertexDots ?
-        polygonList.map((point, i) =>
-          <VertexDot circleRadius={1} key={i} point={point}
-            radius={width} punto={i} />
-        )
-        : null}
+      }
+      
+      {getData()}
       {
         pointText.map((point, i) =>
           <VerTex key={i} point={point} text={data[i].title}
           />
         )
       }
-      {
-        console.log(segment)
-
-      }
-      <GenerateNumber segment={segment} list={[1, 2, 3]} />
+      <GenerateNumber segment={segment} list={list} />
+      <Polygon  points={pointListSegement.join(' ')}
+          color={"#0079ff94"}
+          classCss="polygon-area" />
     </svg>
   )
 };
 const GenerateNumber = ({ segment, list }) => {
-  let count = list.length;
-   
-  return<>
-  {
-    segment.map((point, i) => {
-      let number;
-      if (count == 0) {
-        number = 0;
-      } else {
-        number = list[count];
-      }
-      count++;
-      return <text
-        className="textTilte"
-        x={point[0]} y={point[1]}
-        orientation="outer" stroke="none">{number}
-      </text>
-    })
-  }
+  let count = 0;
+  const initial = () => {
+    const point = segment[segment.length - 1];
+    if (count == 0) return <text
+      className="diagram-text"
+      x={point[0] - 9} y={point[1] + 2}
+      orientation="outer" stroke="none">0
+    </text>
+
+  };
+  return <>
+    {
+      initial()
+    }
+    {
+      segment.map((point, i) => {
+        const reversed = list.reverse();
+        let number;
+        number = reversed[count];
+        count++;
+        return <text
+          key={i}
+          className="diagram-text"
+          x={point[0] - 9} y={point[1] + 2}
+          orientation="outer" stroke="none">{number}
+        </text>
+      })
+    }
   </>
 }
 
@@ -273,7 +216,7 @@ const VerTex = ({
 }) => {
   return (
     <text
-      className="textTilte"
+      className="diagram-text"
       x={point[0]} y={point[1]}
       orientation="outer" stroke="none">{text}
     </text>
@@ -284,8 +227,7 @@ const VerTex = ({
 
 const VertexDot = ({
   circleRadius,
-  point,
-  punto
+  point
 }) => {
   return (
     <circle
@@ -316,10 +258,11 @@ const VertexLine = ({
 
 const Polygon = ({
   points,
-  color
+  color,
+  classCss
 }) => {
 
   return (
-    <polygon className='polygon' fill={color} points={points} />
+    <polygon className={"polygon " + classCss} fill={color} points={points} />
   )
 }
